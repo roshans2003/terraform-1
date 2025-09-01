e4431ab9fd86cdea3ca73c02d1580a6b8773ee0c
roshans2003 <muhammadroshan5199@gmail.com>
2025-09-01 15:12:03 +0530

    version1

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "test" {
+  ami           = "ami-0ed981b8451912647" # valid in ap-south-1
+  instance_type = "t3.micro"
+  key_name      = "windows_key_1"
+  tags = {
+    Name = "TestInstance"
+  }
+}
