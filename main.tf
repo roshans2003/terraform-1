df5329a79cc1eb335ab942e0f017da7f6a1f0a4e
roshans2003 <muhammadroshan5199@gmail.com>
2025-09-01 15:37:36 +0530

    version2

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 resource "aws_instance" "test" {
-  ami           = "ami-0ed981b8451912647" # valid in ap-south-1
+  ami           = "ami-0ed981b8451912647"
   instance_type = "t3.micro"
   key_name      = "windows_key_1"
   tags = {
